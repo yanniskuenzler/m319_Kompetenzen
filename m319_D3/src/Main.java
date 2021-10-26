1ba47b1fcd70ab71e1ef8213e39689d480c03a0e
Yannis Künzler <y.kuenzler@icloud.com>
2021-10-26 11:42:51 +0200

    D3 funktion hinzugefügt

--- m319_D3/src/Main.java
+++ m319_D3/src/Main.java
@@ -13,9 +13,42 @@ public class Main {
 
         int count = 0;
         for (int i = 0; i < input.length; i++) {
-            if (searchword.equals(input[i])) {count++;}
+            if (searchword.equals(input[i])) {
+                count++;
+                input[i] = "";
+            }
         }
 
         System.out.println("Found the word " + count + " times");
+
+        for (int i = 0; i < input.length; i++) {
+            if (!input[i].equals("")) {
+                System.out.print(input[i] + " ");
+            }
+        }
+
+        /*
+            (:) (:)
+               C
+            (_____)
+        */
+
+        //replacement : optional ;)
+        System.out.println();
+        System.out.println("With what do you want to replace the word?\n");
+        String replacement = sc.nextLine();
+
+        for (int i = 0; i < input.length; i++) {
+            if (input[i].equals("")) {
+                input[i] = replacement;
+            }
+        }
+        System.out.println();
+        for (int i = 0; i < input.length; i++) {
+            if (!input[i].equals("")) {
+                System.out.print(input[i] + " ");
+            }
+        }
+
     }
 }
