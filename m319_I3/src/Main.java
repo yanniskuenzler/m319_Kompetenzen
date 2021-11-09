2065c95d2d8413452e70c7b1ba26aa4fe76d5f5f
Yannis Künzler <y.kuenzler@icloud.com>
2021-11-09 09:04:19 +0100

    I3 beendet

---
+++ m319_I3/src/Main.java
@@ -0,0 +1,21 @@
+import java.util.Scanner;
+
+public class Main {
+    public static void main(String[] args) {
+
+        checklist cl = new checklist();
+
+        System.out.println("Willkommen zur ToDo-Liste von Yannis Künzler!\n\nMan kann hier seine Tasks, die man erledigen möchte eintragen und dann, im Erledigen-Modus als erledigt markieren.");
+
+        //Eingabe von Aufgaben, die zu lösen sind
+        cl.insert();
+
+        System.out.println("Erledigen Sie nun Ihre Aufgaben. Immer wenn Sie etwas gemacht haben, geben Sie die Nummer der Aufgabe ein.\nWenn Sie alles gemacht haben oder einfach nur das Programm beenden wollen, schreiben Sie 'exit' und bestätigen Sie mit Enter.\n\nWICHTIG: Wenn das Programm einmal beendet wurde lässt es sich nicht wieder starten und die Daten sind verloren.");
+
+        //Erledigen der Aufgaben
+
+        cl.done();
+
+
+    }
+}
---
+++ m319_I3/src/checklist.java
@@ -0,0 +1,47 @@
+import java.util.Scanner;
+
+public class checklist {
+    String[] toDoList = new String[100]; //Array mit zu erledigenden Aufgaben
+    int actCount = -1; //Anzahl der sich im Array befindenden Elemente
+    String chMode = "n"; //wechseln des Modus
+    boolean ovwr = false; //Überschreiben des letzten Elements im Array
+    String taskComplete = ""; //Eingabe welche Aufgaben als Erledigt markiert werden soll
+
+    Scanner sc = new Scanner(System.in);
+
+    //Eingaben von Aufgaben
+    public void insert() {
+        do {
+            if (ovwr == false) {
+                actCount++;
+            }
+
+            ovwr = false;
+            toDoList[actCount] = sc.nextLine();
+
+            if (toDoList[actCount].equals("")) {
+                System.out.println("Wollen Sie in den Erledigen-Modus gehen, um Ihre Aufgaben zu erledigen [y] oder wollen Sie weitere Tasks eingeben [n]?");
+                chMode = sc.nextLine();
+                ovwr = true;
+            } else {
+                toDoList[actCount] = Integer.toString(actCount) + ": " + toDoList[actCount];
+            }
+        } while ((chMode.equals("n")) && (actCount < 100));
+
+    }
+
+    //Erledigen von Aufgaben
+    public void done() {
+        do {
+            for (int i = 0; i <= actCount; i++) {
+                System.out.println(toDoList[i]);
+            }
+            taskComplete = sc.nextLine();
+
+            if (!taskComplete.equals("exit")) {
+                toDoList[Integer.parseInt(taskComplete)] = "DONE -> " + toDoList[Integer.parseInt(taskComplete)];
+            }
+
+        } while (!taskComplete.equals("exit"));
+    }
+}
